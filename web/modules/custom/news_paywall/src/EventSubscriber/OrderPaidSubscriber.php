d3e1bd6be2f60e5b0c62a5c51393caf423143354
Melanie Kuhles <melanie.kuhles@oder-spree-consulting.de>
2026-01-16 14:22:45 +0100

    replace hardcoded role

--- web/modules/custom/news_paywall/src/EventSubscriber/OrderPaidSubscriber.php
+++ web/modules/custom/news_paywall/src/EventSubscriber/OrderPaidSubscriber.php
@@ -6,16 +6,23 @@ use Drupal\commerce_order\Event\OrderEvent;
 use Drupal\commerce_order\Event\OrderEvents;
 use Drupal\user\UserInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Logger\LoggerChannelInterface;
 
 /**
  * Subscribes to order paid events to grant entitlements.
  *
  * Whenever an order is marked as paid, this subscriber grants the purchasing
- * user the 'subscriber' role. This simple implementation can be extended to
- * record additional entitlement information (e.g. license expiry dates or
- * subscription IDs).
+ * user the 'news_paywall_subscriber' role. This simple implementation can be
+ * extended to record additional entitlement information (e.g. license expiry
+ * dates or subscription IDs).
  */
 final class OrderPaidSubscriber implements EventSubscriberInterface {
+
+  public function __construct(
+    private readonly ConfigFactoryInterface $configFactory,
+    private readonly LoggerChannelInterface $logger,
+  ) {}
 
   /**
    * {@inheritdoc}
@@ -27,7 +34,7 @@ final class OrderPaidSubscriber implements EventSubscriberInterface {
   }
 
   /**
-   * Handles order paid events to grant the 'subscriber' role.
+   * Handles order paid events to grant the 'news_paywall_subscriber' role.
    *
    * @param \Drupal\commerce_order\Event\OrderEvent $event
    *   The order event.
@@ -41,8 +48,15 @@ final class OrderPaidSubscriber implements EventSubscriberInterface {
       return;
     }
 
-    if (!$customer->hasRole('subscriber')) {
-      $customer->addRole('subscriber');
+    $role_id = $this->configFactory
+      ->get('news_paywall.settings')
+      ->get('grant_role_id') ?: 'news_paywall_subscriber';
+    if (!$role_id) {
+      $this->logger->error('No role ID configured to grant upon order payment.');
+      return;
+    }
+    if (!$customer->hasRole($role_id)) {
+      $customer->addRole($role_id);
       $customer->save();
     }
   }
